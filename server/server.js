e05b403e22b39a962390804ba02f7d7f52be6aed
Rishabh Budhiraja <rishabh.budhiraja@gmail.com>
2018-07-04 21:57:59 +0530

    authentiaction proper

--- server/server.js
+++ server/server.js
@@ -20,12 +20,6 @@ const fs = require('fs');
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', "*");
-    res.header('Access-Control-Allow-Methods','GET,PUT,POST,DELETE');
-    res.header('Access-Control-Allow-Headers', 'Content-Type');
-    next();
-});
 
 app.set('view engine', 'hbs');
 
