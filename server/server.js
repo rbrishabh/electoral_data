8843e04ca175a1262696e6073d6803bebff7cbcf
Rishabh Budhiraja <rishabh.budhiraja@gmail.com>
2018-07-04 11:31:08 +0530

    Heroku

--- server/server.js
+++ server/server.js
@@ -20,18 +20,20 @@ const fs = require('fs');
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.set('view engine', 'hbs');
-
-app.use(express.static(__dirname+'./../views/img'));
-app.use(express.static(__dirname+'./../views/hbsJS'));
-app.use(bodyParser.urlencoded({ extended: false }));
-
 app.use(function(req, res, next) {
     res.header('Access-Control-Allow-Origin', "*");
     res.header('Access-Control-Allow-Methods','GET,PUT,POST,DELETE');
     res.header('Access-Control-Allow-Headers', 'Content-Type');
     next();
 });
+
+app.set('view engine', 'hbs');
+
+app.use(express.static(__dirname+'./../views/img'));
+app.use(express.static(__dirname+'./../views/hbsJS'));
+app.use(bodyParser.urlencoded({ extended: false }));
+
+
 
 app.use(session({
     secret: 'work hard',
