17be29fc2d94030a6c9c5cdccc680bdf6a9bf7d8
Rishabh Budhiraja <rishabh.budhiraja@gmail.com>
2018-07-10 16:59:16 +0530

    small improvements

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 const urlLocal = 'mongodb://localhost:27017/electoralDatabase';
-const database =  process.env.URL;
+const database =  process.env.URI;
 var db = mongoose.connection;
 
 //handle mongo error
