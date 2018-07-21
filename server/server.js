51ad3331eca63ed18c95ba2c83157cfe2e48e81a
Rishabh Budhiraja <rishabh.budhiraja@gmail.com>
2018-07-21 20:28:03 +0530

    trying

--- server/server.js
+++ server/server.js
@@ -30,7 +30,7 @@ app.use(express.static(__dirname+'./../views/hbsJS'));
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', "https://ems-data.herokuapp.com/*");
+    res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods','GET,PUT,POST,DELETE');
     res.header('Access-Control-Allow-Headers', 'Content-Type');
     next();
